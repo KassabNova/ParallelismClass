80e317d54a6babe260280e10d2b7f07294da2ae0
Kassab <CKassabAndre@gmail.com>
2020-03-12 08:37:33 -0600

    Finished

--- Term3/Projects/PPOT/PPOT.cs
+++ Term3/Projects/PPOT/PPOT.cs
@@ -8,17 +8,20 @@ namespace PPOT
     class PPOT
     {
         static List<Player> players = new List<Player>();
-        static short rounds = 1;
+        static byte rounds = 30;
         private static readonly RNGCryptoServiceProvider _generator = new RNGCryptoServiceProvider();
         public static Object waitObject = new Object();
         public static Object waitObject2 = new Object();
-        static int ready = 0;
+        static byte ready = 0;
         static void Main(string[] args)
         {
             Thread player1 = new Thread(() => PlayerLoop("Player1"));
             Thread player2 = new Thread(() => PlayerLoop("Player2"));
             Thread player3 = new Thread(() => PlayerLoop("Player3"));
-
+            string input;
+            Console.Write("Numero de rondas:");
+            input = Console.ReadLine();
+            byte.TryParse(input, out rounds);
             player1.Start();
             player2.Start();
             player3.Start();
@@ -26,7 +29,7 @@ namespace PPOT
             player1.Join();
             player2.Join();
             player3.Join();
-
+                       
         }
         static void PlayerLoop(string name)
         {
@@ -34,14 +37,14 @@ namespace PPOT
             lock (waitObject)
             {
                 players.Add(player);
+                players.Sort((player1,player2) => player1.name.CompareTo(player2.name));
             }
 
             GameLoop(player);
         }
         static void GameLoop(Player player)
         {
-
-            for (int i = 0; i < rounds; i++)
+            for (byte i = 0; i < rounds; i++)
             {
                 lock (waitObject) ready++;
                 Choice choice;
@@ -50,7 +53,7 @@ namespace PPOT
                 =====================================*/
                 if (ready >= 3)
                 {
-                    Console.WriteLine($"Todos listos ({ready}) para la ronda {i + 1}");
+                    Console.WriteLine($"\n\nTodos listos ({ready}) para la ronda {i + 1}");
                     ready = 0; //resetting for next round
                     lock (waitObject)
                     {
@@ -62,7 +65,7 @@ namespace PPOT
                 {
                     lock (waitObject)
                     {
-                        Console.WriteLine($"{player.name} esperando a que empieze ronda {i + 1}");
+                        //Console.WriteLine($"{player.name} esperando a que empieze ronda {i + 1}");
                         Monitor.Wait(waitObject);
                     }
                 }
@@ -75,8 +78,9 @@ namespace PPOT
                 {
                     if (playerList.name == player.name)
                     {
+                        player.victories = 0; //resetting battle round victories
                         choice = playerList.Choose();
-                        Console.WriteLine($"{player.name} choose {Enum.GetName(typeof(Choice), choice)} {ready}");
+                        Console.WriteLine($"{player.name} choose {Enum.GetName(typeof(Choice), choice)}");
                         lock (waitObject) ready++;
                         
                     }
@@ -110,13 +114,21 @@ namespace PPOT
                 {
                     Console.WriteLine($"Sacando resultados");
                     ready = 0; //resetting for next round
+
                     foreach(Player player1 in players)
                     {
                         foreach(Player player2 in players)
                         {
-                            Player winner = player1.Battle(player2);
+                            if(player1.name != player2.name)
+                            {
+                                Player winner = player1.Battle(player2);
+                            }
                         }
                     }
+
+                    //Finally
+                    AssignPoints();//Give points based on requirements
+                    //And Then Continue
                     lock (waitObject)
                     {
                         //Console.WriteLine($"{player.name} avisa que todos escogieron");
@@ -131,21 +143,49 @@ namespace PPOT
                         Monitor.Wait(waitObject);
                     }
                 }
-
-                //Resolucion de ganadores
-
             }
         }
         //Un jugador recibirá dos puntos si bate a sus otros dos competidores.
         //Dos jugadores recibirán un punto cada uno si entre ellos baten a un tercero.
         //Cualquier otro caso no reciben puntos.Luego los jugadores juegan otra partida.
- 
+        public static byte AssignPoints()
+        {
+            //ToDo Change this so it can be dynamic
+            //Resolucion de ganadores
+            if (players[0].victories == 1 && players[1].victories == 1 && players[2].victories == 1)
+            {
+                Console.WriteLine("Todos ganaron una ronda, asi que 0 puntos!");
+                PrintScores();
+                return 0;
+            }
+            if (players[0].victories == 0 && players[1].victories == 0 && players[2].victories == 0)
+            {
+                Console.WriteLine("Todos perdieron todas las rondas, asi que 0 puntos!");
+                PrintScores();
+                return 0;
+            }
+            foreach(Player player in players)
+            {
+                player.points += player.victories;
+                player.victories = 0; //resetting battle round victories
+            }
+            PrintScores();
+            return 0;
+
+        }
+        public static void PrintScores()
+        {
+            foreach (Player player in players)
+            {
+                Console.WriteLine($"Score del {player.name}: {player.points}");
+            }
+        }
         class Player
         {
             public string name { get; set; }
-            public int victories { get; set; }
+            public byte victories { get; set; }
+            public byte points { get; set; }
             public Choice ppot { get; set; }
-
             public Player(string name)
             {
                 this.name = name;
@@ -157,10 +197,29 @@ namespace PPOT
             }
             public Player Battle(Player battler)
             {
-
+                if(this.ppot == Choice.Piedra && battler.ppot == Choice.Papel)
+                {
+                    battler.victories++;
+                    return battler;
+                }
+                if (this.ppot == Choice.Papel && battler.ppot == Choice.Tijera)
+                {
+                    battler.victories++;
+                    return battler;
+                }
+                if (this.ppot == Choice.Tijera && battler.ppot == Choice.Piedra)
+                {
+                    battler.victories++;
+                    return battler;
+                }
+                if (this.ppot == battler.ppot )
+                {
+                    return null;
+                }
                 return this;
             }
-        }=        enum Choice
+        }
+        enum Choice
         {
             Piedra = 1,
             Papel,
@@ -188,21 +247,4 @@ namespace PPOT
         }
     }
 }
-/*
- 
-Cada jugador de manera aleatoria escogerá piedra, papel o tijera.
-Luego los jugadores compararán sus opciones y verán quien ganó la partida.
 
-Un jugador recibirá dos puntos si bate a sus otros dos competidores.
-Dos jugadores recibirán un punto cada uno si entre ellos baten a un tercero.
-Cualquier otro caso no reciben puntos. Luego los jugadores juegan otra partida.
-
-Utilice un hilo para simular a cada jugador.
-Los jugadores pueden interactuar  directamente con cada uno de ellos.
-
-Su programa deberá preguntar antes de iniciar la partida el número de partidas totales a jugar. 
-El programa deberá mostrar una traza de los resultados de cada partida conforme avanza.
-
-Al final del juego su programa deberá imprimir el total de puntos obtenidos por cada jugador. 
-
-     */
