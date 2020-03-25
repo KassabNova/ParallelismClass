eaede17c35b5ad43770202e895ac4cb702fe47f6
Kassab <CKassabAndre@gmail.com>
2020-03-25 16:34:04 -0600

    BubbleSort with Merge. Much Faster now

--- BubbleSort/FinalProyect.cs
+++ BubbleSort/FinalProyect.cs
@@ -10,7 +10,7 @@ namespace BubbleSort
     {
 
         private static  int MAXELEMS = 10000000;
-        private static int DEFELEMS =  178000;
+        private static int DEFELEMS = 200000;
         private static int MINELEMS = 0;
 		private static readonly RNGCryptoServiceProvider _generator = new RNGCryptoServiceProvider();
 		private const int maxNumber = 100;
@@ -82,25 +82,32 @@ namespace BubbleSort
 			int i, j;
 			int temp;
 
-			//Cuarto nivel
+			//Cuarto nivel lado izquierdo
 			Thread t1 = new Thread(() => BubbleSort(0, elems / 8, inicio));
 			Thread t2 = new Thread(() => BubbleSort(elems / 8, elems / 8 * 2, inicio));
 			Thread t3 = new Thread(() => BubbleSort(elems / 8 * 2, elems / 8 * 3, inicio));
 			Thread t4 = new Thread(() => BubbleSort(elems / 8 * 3, elems/2, inicio));
+
+			//Cuarto nivel lado derecho
 			Thread t5 = new Thread(() => BubbleSort(elems / 2, elems / 8 * 5, inicio));
 			Thread t6 = new Thread(() => BubbleSort(elems / 8 * 5, elems / 8 * 6, inicio));
 			Thread t7 = new Thread(() => BubbleSort(elems / 8 * 6, elems / 8 * 7, inicio));
 			Thread t8 = new Thread(() => BubbleSort(elems / 8 * 7, elems, inicio));
-			//Tercer nivel
-			Thread t09 = new Thread(() => BubbleSort(0, elems / 4, inicio));
-			Thread t10 = new Thread(() => BubbleSort(elems / 4, elems / 2, inicio));
-			Thread t11 = new Thread(() => BubbleSort(elems / 2, elems / 4 * 3, inicio));
-			Thread t12 = new Thread(() => BubbleSort(elems / 4 * 3, elems, inicio));
-			//Segundo nivel							  
-			Thread t13 = new Thread(() => BubbleSort(0, elems / 2, inicio));
-			Thread t14 = new Thread(() => BubbleSort(elems / 2, elems, inicio));
-			//Primer nivel							  
-			Thread t15 = new Thread(() => BubbleSort(0, elems, inicio));
+
+			//Tercer nivel lado izquierdo
+			Thread t09 = new Thread(() => MergeSort(inicio, 0, elems / 4));
+			Thread t10 = new Thread(() => MergeSort(inicio, elems / 4, elems / 2));
+
+			//Tercer nivel lado derecho
+			Thread t11 = new Thread(() => MergeSort(inicio, elems / 2, elems / 4 * 3));
+			Thread t12 = new Thread(() => MergeSort(inicio , elems / 4 * 3, elems - 1));
+
+			//Segundo nivel					  
+			Thread t13 = new Thread(() => MergeSort(inicio, 0, elems / 2)); //lado izquierdo
+			Thread t14 = new Thread(() => MergeSort(inicio, elems / 2, elems - 1)); //lado derecho
+
+			//Primer nivel => Juntando lo último
+			Thread t15 = new Thread(() => MergeSort(inicio, 0, elems - 1));
 
 			//Cuarto nivel
 			t1.Start();
@@ -139,27 +146,9 @@ namespace BubbleSort
 			t13.Join();
 			t14.Join();
 
-
-
 			//Primer nivel
 			t15.Start();
 			t15.Join();
-
-			//for (i = 0; i < elems - 1; i++)
-			//{
-			//	for (j = i + 1; j < elems; j++)
-			//	{
-
-			//		if (inicio[i] > inicio[j])
-			//		{
-			//			temp = inicio[i];
-			//			inicio[i] = inicio[j];
-			//			inicio[j] = temp;
-			//		}
-
-			//	}
-			//}
-
 
 			return;
 		}
@@ -184,29 +173,53 @@ namespace BubbleSort
 				}
 			}
 		}
-		static void SeeSharp(int start, int end, int[] inicio)
+		static void Merge(int[] input, int left, int middle, int right)
 		{
-			int sum = 0;
-			int temp;
-			int i, j;
-			for (i = start; i < end - 1; i++)
+			int[] leftArray = new int[middle - left + 1];
+			int[] rightArray = new int[right - middle];
+
+			Array.Copy(input, left, leftArray, 0, middle - left + 1);
+			Array.Copy(input, middle + 1, rightArray, 0, right - middle);
+
+			int i = 0;
+			int j = 0;
+			for (int k = left; k < right + 1; k++)
 			{
-
-				for (j = i + 1; j < end; j++)
+				if (i == leftArray.Length)
 				{
-
-					if (inicio[i] > inicio[j])
-					{
-						temp = inicio[i];
-						inicio[i] = inicio[j];
-						inicio[j] = temp;
-					}
-
+					input[k] = rightArray[j];
+					j++;
+				}
+				else if (j == rightArray.Length)
+				{
+					input[k] = leftArray[i];
+					i++;
+				}
+				else if (leftArray[i] <= rightArray[j])
+				{
+					input[k] = leftArray[i];
+					i++;
+				}
+				else
+				{
+					input[k] = rightArray[j];
+					j++;
 				}
 			}
 		}
+		static void MergeSort(int[] input, int left, int right)
+		{
+			if (left < right)
+			{
+				int middle = (left + right) / 2;
 
-		
+				MergeSort(input, left, middle);
+				MergeSort(input, middle + 1, right);
+
+				Merge(input, left, middle, right);
+			}
+		}
+		//Funcion para sacar números random más random
 		public static int Between(int minimumValue, int maximumValue)
 		{
 			byte[] randomNumber = new byte[1];
