981671f60e7f032cf04e0c7a3836b8e8a9991912
Kassab <CKassabAndre@gmail.com>
2020-03-25 16:37:36 -0600

    Simple BubbleSort

--- BubbleSort/FinalProyect.cs
+++ BubbleSort/FinalProyect.cs
@@ -79,77 +79,24 @@ namespace BubbleSort
 
 		public static void sort(int[] inicio, int elems)
 		{
-			int i, j;
+			int sum = 0;
 			int temp;
+			int i, j;
+			for (i = 0; i < elems - 1; i++)
+			{
 
-			//Cuarto nivel lado izquierdo
-			Thread t1 = new Thread(() => BubbleSort(0, elems / 8, inicio));
-			Thread t2 = new Thread(() => BubbleSort(elems / 8, elems / 8 * 2, inicio));
-			Thread t3 = new Thread(() => BubbleSort(elems / 8 * 2, elems / 8 * 3, inicio));
-			Thread t4 = new Thread(() => BubbleSort(elems / 8 * 3, elems/2, inicio));
+				for (j = i + 1; j < elems; j++)
+				{
 
-			//Cuarto nivel lado derecho
-			Thread t5 = new Thread(() => BubbleSort(elems / 2, elems / 8 * 5, inicio));
-			Thread t6 = new Thread(() => BubbleSort(elems / 8 * 5, elems / 8 * 6, inicio));
-			Thread t7 = new Thread(() => BubbleSort(elems / 8 * 6, elems / 8 * 7, inicio));
-			Thread t8 = new Thread(() => BubbleSort(elems / 8 * 7, elems, inicio));
+					if (inicio[i] > inicio[j])
+					{
+						temp = inicio[i];
+						inicio[i] = inicio[j];
+						inicio[j] = temp;
+					}
 
-			//Tercer nivel lado izquierdo
-			Thread t09 = new Thread(() => MergeSort(inicio, 0, elems / 4));
-			Thread t10 = new Thread(() => MergeSort(inicio, elems / 4, elems / 2));
-
-			//Tercer nivel lado derecho
-			Thread t11 = new Thread(() => MergeSort(inicio, elems / 2, elems / 4 * 3));
-			Thread t12 = new Thread(() => MergeSort(inicio , elems / 4 * 3, elems - 1));
-
-			//Segundo nivel					  
-			Thread t13 = new Thread(() => MergeSort(inicio, 0, elems / 2)); //lado izquierdo
-			Thread t14 = new Thread(() => MergeSort(inicio, elems / 2, elems - 1)); //lado derecho
-
-			//Primer nivel => Juntando lo último
-			Thread t15 = new Thread(() => MergeSort(inicio, 0, elems - 1));
-
-			//Cuarto nivel
-			t1.Start();
-			t2.Start();
-			t3.Start();
-			t4.Start();
-			t5.Start();
-			t6.Start();
-			t7.Start();
-			t8.Start();
-
-			t1.Join();
-			t2.Join();
-			t3.Join();
-			t4.Join();
-			t5.Join();
-			t6.Join();
-			t7.Join();
-			t8.Join();
-
-			//Tercer nivel
-			t09.Start();
-			t10.Start();
-			t11.Start();
-			t12.Start();
-
-			t09.Join();
-			t10.Join();
-			t11.Join();
-			t12.Join();
-
-			//Segundo nivel
-			t13.Start();
-			t14.Start();
-
-			t13.Join();
-			t14.Join();
-
-			//Primer nivel
-			t15.Start();
-			t15.Join();
-
+				}
+			}
 			return;
 		}
 		static void BubbleSort(int start, int end, int[] inicio)
